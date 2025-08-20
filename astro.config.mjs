5e65aeb1b1b33e42a40dae4d82dc0bad17d54f50
Peter Promok <peterpromok888@gmail.com>
2025-08-20 11:47:42 +0200

    Refactor astro.config.mjs for consistent formatting and add build command to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://promok.tech',
+  site: "https://promok.tech",
+  build: {
+    assets: "assets",
+  },
 });
