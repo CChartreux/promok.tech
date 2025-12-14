541cd67c8136cb968417c301cb6d98b629362b78
Peter Promok <peterpromok888@gmail.com>
2025-12-14 12:11:11 +0100

    🔄 refactor(forecast): rename ForecastWidgetService to ForecastWidget for clarity and consistency

--- site/src/jsMain/kotlin/com/promok/tech/app/workstation/components/lockscreen/widget/forecast/ForecastWidget.kt
+++ site/src/jsMain/kotlin/com/promok/tech/app/workstation/components/lockscreen/widget/forecast/ForecastWidget.kt
@@ -3,7 +3,7 @@ package com.promok.tech.app.workstation.components.lockscreen.widget.forecast
 import kotlinx.browser.window
 import kotlinx.coroutines.await
 
-object ForecastWidgetService {
+object ForecastWidget {
     suspend fun getWeather(): String {
         val url =
             "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
--- site/src/jsMain/kotlin/com/promok/tech/app/workstation/components/lockscreen/widget/forecast/ForecastWidgetComponent.kt
+++ site/src/jsMain/kotlin/com/promok/tech/app/workstation/components/lockscreen/widget/forecast/ForecastWidgetComponent.kt
@@ -3,7 +3,7 @@ package com.promok.tech.app.workstation.components.lockscreen.widget.forecast
 import androidx.compose.runtime.*
 import com.promok.tech.app.workstation.components.Components
 import com.promok.tech.app.workstation.components.lockscreen.widget.WidgetComponent
-import com.promok.tech.app.workstation.components.lockscreen.widget.forecast.ForecastWidgetService.getWeather
+import com.promok.tech.app.workstation.components.lockscreen.widget.forecast.ForecastWidget.getWeather
 import org.jetbrains.compose.web.dom.Text
 
 object ForecastWidgetComponent : Components {
